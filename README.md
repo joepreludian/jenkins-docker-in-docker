b04a2cf782a7e82b1f696eb31790a8329796724f
Jonhnatha Trigueiro <joepreludian@gmail.com>
2018-12-26 18:58:31 -0200

    Update on the codebase; Improvements on Readme file

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# jenkins-docker-in-docker
-A Jenkins LTS distribution made for being used inside a Docker container, but managing the docker host.
+# Preludian Jenkins Docker in Docker
+
+Simple tool to install and run a Jenkins Docker-in-Docker for your computer or a production server.
+
+# What is Docker-in-Docker?
+
+Docker-in-Docker is an initiative to use a Dockerized version of Docker with the Jenkins container being able to see the host docker socket, manipulating it.
+The main objective is to keep Jenkins as a clean, but battle tested for any kind of build, with any technology/version.
+
+Suppose that you need to create a job that builds a Java 1.6 application. With this approach you just need to create a docker container which runs Java 1.6 instead of letting jenkins handle this for you by using the Jenkins Docker pipelines.
+But the thing is: ok, I have a Jenkins job, but how can I look inside the workspace and how to instantiate a new job and have my workspace visible inside the new container?
+
+The answer is on the container itself. You must ensure that the /var/jenkins\_home will be accessible inside your container. For expliciting this /var/jenkins\_home inside the container the jenkins itself will chdir into the correct place, making your workspace folder visible inside the container;
+
+# What do we have in the box?
+
+We have a ready to use Jenkins with latest version of the war file and a bunch of plugins installed.
+
+# How to install it?
+
+To use this project you must have a docker installed. I tested with the latest available version so far: Docker CE 18+, but it might work on previous versions as well.
+
+In a nutshell, to install a jenkins server you must clone this repo, then run the following command:
+
+```
+DOCKER_PREFIX=my_brand_new_jenkins DOCKER_PROJECT_PORT=8080 ./jenkins_deployment.sh install
+```
+
+## Params
+
+In order to create an automated installation, I used the environment variables approach;
+You must set the following params:
+
+* DOCKER\_PREFIX - will be the name of your project;
+* DOCKER\_PROJECT\_PORT - It will be the port number that I will use to export your jenkins to the Docker host;
+* DOCKER\_AUTOYES - if set as 1 all prompts will be assumed as Yes, useful for automated environments;
+
+## What happens behind the stage?
+
+It will, in a nutshell:
+
+1. Create the docker volumes for Jenkins home and Jenkins core;
+2. Pull the Jenkins image and RUN;
+3. Create a container mounting the volumes previously created with the docker mountpoint;
+4. Skipping the initial Jenkins installation, proceeding to install the plugins automatically;
+5. Performing a preflight check;
+6. Sending you your admin password so you will being able to use your brand new Jenkins!
+
+
+# Uninstalling
+
+To uninstall it's pretty simple. Just trigger the following command:
+
+```
+DOCKER_PREFIX=my_brand_new_jenkins DOCKER_PROJECT_PORT=8080 ./jenkins_deployment.sh uninstall
+```
+
+This command will remove both jenkins container, and their associated images;
+
+
+# Contribute!
+
+Feel free to raise a pull request or an issue! It's made with pure bash script in order to be as more compatible as possible.
+This is a ugly version of the script, but it is functional. I promisse to enhance it with the time and the adoption of more users.
+
+Thank you and enjoy!
--- jenkins_deployment.sh
+++ jenkins_deployment.sh
@@ -2,7 +2,6 @@
 
 JENKINS_DOCKER_IMAGE="jenkins/jenkins:lts"
 
-
 function review_configuration {
   if [ -z "${DOCKER_PREFIX}" ] 
   then 
@@ -55,19 +54,19 @@ function raise_docker_volumes {
 
   echo "Fetching docker Volumes...";
 
-  docker volume inspect $DOCKER_VOLUME_JENKINS_HOME || CREATE_DOCKER_VOLUME_JENKINS_HOME=1;
-  docker volume inspect $DOCKER_VOLUME_JENKINS_CORE || CREATE_DOCKER_VOLUME_JENKINS_CORE=1;
+  docker volume inspect $DOCKER_VOLUME_JENKINS_HOME &> /dev/null || CREATE_DOCKER_VOLUME_JENKINS_HOME=1;
+  docker volume inspect $DOCKER_VOLUME_JENKINS_CORE &> /dev/null || CREATE_DOCKER_VOLUME_JENKINS_CORE=1;
 
   if [ -n "${CREATE_DOCKER_VOLUME_JENKINS_HOME}" ]
   then
     echo "* Creating JENKINS_HOME Volume";
-    docker volume create $DOCKER_VOLUME_JENKINS_HOME;
+    docker volume create $DOCKER_VOLUME_JENKINS_HOME &> /dev/null;
   fi
 
   if [ -n "${CREATE_DOCKER_VOLUME_JENKINS_CORE}" ]
   then
     echo "* Creating JENKINS_CORE Volume";
-    docker volume create $DOCKER_VOLUME_JENKINS_CORE;
+    docker volume create $DOCKER_VOLUME_JENKINS_CORE &> /dev/null;
   fi
 }
 
@@ -88,15 +87,14 @@ function install_jenkins_container {
 
   echo "- NEW JENKINS WAR FILE: ${JENKINS_URL}";
 
-  echo "Raising Jenkins Container";
-  docker inspect $DOCKER_PREFIX || CREATE_DOCKER_INSTANCE=1;
+  echo "Checking if Jenkins Container Exists...";
+  docker inspect $DOCKER_PREFIX &> /dev/null || CREATE_DOCKER_INSTANCE=1;
 
   if [ -n "${CREATE_DOCKER_INSTANCE}" ]
   then
-    echo "Creating instance...";
-    docker run --restart=always -d -m $DOCKER_PROJECT_MEMORY_USAGE --name $DOCKER_PREFIX -v $DOCKER_VOLUME_JENKINS_HOME:/var/jenkins_home -v $DOCKER_VOLUME_JENKINS_CORE:/usr/share/jenkins -v /var/run/docker.sock:/var/run/docker.sock -it -p ${DOCKER_PROJECT_PORT}:8080 $JENKINS_DOCKER_IMAGE;
+    echo "Creating the Jenkins Container...";
+    docker run --restart=always -d -m $DOCKER_PROJECT_MEMORY_USAGE --name $DOCKER_PREFIX -v $DOCKER_VOLUME_JENKINS_HOME:/var/jenkins_home -v $DOCKER_VOLUME_JENKINS_CORE:/usr/share/jenkins -v /var/run/docker.sock:/var/run/docker.sock -it -p ${DOCKER_PROJECT_PORT}:8080 $JENKINS_DOCKER_IMAGE &> /dev/null;
     
-    echo "Waiting a little...";
     sleep 8;
 
     echo "Downloading latest jenkins war file into /usr/share/jenkins...";
@@ -105,31 +103,34 @@ function install_jenkins_container {
     echo "Injecting Jenkins CLI...";
     inject_cli;
 
-   echo "Restart container...";
-    docker restart $DOCKER_PREFIX;
+    echo "Restart container...";
+    docker restart $DOCKER_PREFIX &> /dev/null;
+    sleep 10;
 
   fi
 }
 
 function inject_config {
-    echo "Overriding the installer [NEW to RUNNING] on installStateName at config.xml...";
-    docker exec -it --user root $DOCKER_PREFIX sed -i 's/<installStateName>NEW<\/installStateName>/<installStateName>RUNNING<\/installStateName>/g' /var/jenkins_home/config.xml
-    
-    echo "Injecting Environment variable where is located the jenkins_home docker volume...";
-    docker exec -it --user root $DOCKER_PREFIX sed -i "s/<globalNodeProperties\/>/<globalNodeProperties><hudson.slaves.EnvironmentVariablesNodeProperty><envVars serialization=\"custom\"><unserializable-parents\/><tree-map><default><comparator class=\"hudson.util.CaseInsensitiveComparator\"\/><\/default><int>1<\/int><string>JENKINS_HOME_VOLUME<\/string><string>${DOCKER_VOLUME_JENKINS_HOME}<\/string><\/tree-map><\/envVars><\/hudson.slaves.EnvironmentVariablesNodeProperty><\/globalNodeProperties>/g" /var/jenkins_home/config.xml;
+  sleep 10;
+  echo "Overriding the installer [NEW to RUNNING] on installStateName at config.xml...";
+  docker exec -it --user root $DOCKER_PREFIX sed -i 's/<installStateName>NEW<\/installStateName>/<installStateName>RUNNING<\/installStateName>/g' /var/jenkins_home/config.xml
+  
+  echo "Injecting Environment variable where is located the jenkins_home docker volume...";
+  docker exec -it --user root $DOCKER_PREFIX sed -i "s/<globalNodeProperties\/>/<globalNodeProperties><hudson.slaves.EnvironmentVariablesNodeProperty><envVars serialization=\"custom\"><unserializable-parents\/><tree-map><default><comparator class=\"hudson.util.CaseInsensitiveComparator\"\/><\/default><int>1<\/int><string>JENKINS_HOME_VOLUME<\/string><string>${DOCKER_VOLUME_JENKINS_HOME}<\/string><\/tree-map><\/envVars><\/hudson.slaves.EnvironmentVariablesNodeProperty><\/globalNodeProperties>/g" /var/jenkins_home/config.xml;
 
-    echo "Restarting base container...";
-    docker restart $DOCKER_PREFIX;
+  echo "Restarting base container...";
+  docker restart $DOCKER_PREFIX &> /dev/null;
 }
 
 function inject_cli {
-    docker exec -it --user root $DOCKER_PREFIX wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O /usr/share/jenkins/jenkins_cli.jar;
+  sleep 10;
+  docker exec -it --user root $DOCKER_PREFIX wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O /usr/share/jenkins/jenkins_cli.jar;
 
-    if [ $? -ne 0 ]
-    then
-      echo "An error occured... Trying again...";  # @todo add a rule to set a timeout;
-      inject_cli;
-    fi
+  if [ $? -ne 0 ]
+  then
+    echo "An error occured... Trying again...";  # @todo add a rule to set a timeout;
+    inject_cli;
+  fi
 }
 
 function uninstall {
@@ -139,6 +140,8 @@ function uninstall {
   docker stop $DOCKER_PREFIX; 
   docker rm $DOCKER_PREFIX; 
   docker volume rm $DOCKER_VOLUME_JENKINS_HOME $DOCKER_VOLUME_JENKINS_CORE;
+
+  echo "Done.";
 }
 
 #
@@ -162,10 +165,14 @@ function install {
 
   inject_config;
 
-  echo "Skipping install_plugins...";  #install_plugins;
+  install_plugins;
 
   ADMIN_PASSWORD=$(print_initial_admin_password);
  
+  preflight_check || {
+    echo "There is an error. I suggest that you uninstall prior to re-installing it again.";
+  };
+
   echo;
   echo "Access your ip address at http://localhost:${DOCKER_PROJECT_PORT}/user/admin/configure and put the initial admin password...";
   echo "Username: admin";
@@ -173,23 +180,61 @@ function install {
 
   echo;
 
-  echo "Enjoy your Jenkins!"
+  echo "Enjoy your Jenkins!";
 }
 
 function install_plugins {
-  ADMIN_PASSWORD=$(print_initial_admin_password);
+
+  echo "Waiting a 10s in order to get Jenkins up...";
+  sleep 10;
+
   PLUGIN_LIST=$(cat jenkins_plugin_names | xargs echo);
   PLUGIN_AMOUNT=$(cat jenkins_plugin_names | wc -l);
 
   echo "Installing/Updating a list of ${PLUGIN_AMOUNT} plugins (according \"jenkins_plugin_names\" file)...";
-  docker exec -it --user root $DOCKER_PREFIX java -jar /usr/share/jenkins/jenkins_cli.jar -http -s http://127.0.0.1:8080/ -auth admin:${ADMIN_PASSWORD} install-plugin $PLUGIN_LIST;
+  jenkins_cli install-plugin $PLUGIN_LIST;
 
   echo "Restarting container...";
   docker restart $DOCKER_PREFIX;
+
+  sleep 20;
+  echo "Waiting 20s in order to get Jenkins ready...";
+}
+
+function echo_command_return {
+  EXIT_CODE=$?;
+
+  if [ $EXIT_CODE -eq 0 ]
+  then
+    echo "[SUCCESS]";
+  else
+    echo "[FAIL]";
+    exit $EXIT_CODE;
+  fi
+}
+
+function jenkins_cli {
+  ADMIN_PASSWORD=$(print_initial_admin_password);
+  docker exec -it --user root $DOCKER_PREFIX java -jar /usr/share/jenkins/jenkins_cli.jar -http -s http://127.0.0.1:8080/ -auth admin:${ADMIN_PASSWORD} $@;
+}
+
+function preflight_check {
+  sleep 10;
+  echo "Preflight check!";
+  
+  echo -n " - Checking if site responds - ";
+  curl -q http://localhost:$DOCKER_PROJECT_PORT/ &> /dev/null; echo_command_return || exit $?;
+
+  echo -n " - Getting jenkins version: ";
+  jenkins_cli version || exit $?;
+
+  echo -n " - Installed Plugins";
+  jenkins_cli list-plugins || exit $?;
+
 }
 
 function help {
-  echo "Usage: $0";
+  echo "Please read the README.md file for more information";
 }
 
 echo "PRELUDIAN Jenkins Docker In Docker - Installer - version `cat VERSION`";
@@ -204,5 +249,5 @@ then
 fi
 
 # Perform the action
-$1 || exit $?;
+$@ || exit $?;
 
